614f67224e8e4c1ff5c5102b84b0ec13b19b12bb
liuchengkai <1097163128@qq.com>
2019-12-12 09:43:23 +0800

    a naive modification

--- if_id.v
+++ if_id.v
@@ -4,20 +4,28 @@ module if_id(
     input wire clk,
     input wire rst,
 
+    // from ctrl
+    input wire[`StallBus]       stall,
+
+    // from if
     input wire[`InstAddrBus]    if_pc,
     input wire[`InstBus]        if_inst,
 
+    // to id
     output reg[`InstAddrBus]    id_pc,
     output reg[`InstBus]        id_inst
 );
 
     always @ (posedge clk)  begin
         if (rst == `RstEnable)  begin
-            id_pc <= `ZeroWord;
-            id_inst <= `ZeroWord;
-        end else begin
-            id_pc <= if_pc;
-            id_inst <= if_inst;
+            id_pc       <= `ZeroWord;
+            id_inst     <= `ZeroWord;
+        end else  if (stall[2] == `Stop && stall[3] == `NoStop) begin
+            id_pc       <= `ZeroWord;
+            id_inst     <= `ZeroWord;
+        end else if (stall[2] == `NoStop) begin
+            id_pc       <= if_pc;
+            id_inst     <= if_inst;
         end
     end
 
--- mem_wb.v
+++ mem_wb.v
@@ -4,10 +4,15 @@ module mem_wb(
     input wire      clk,
     input wire      rst,
 
+    // from ctrl
+    input wire[`StallBus]               stall,
+
+    // from mem
     input wire[`RegAddrBus]             mem_wd,
     input wire                          mem_wreg,
     input wire[`RegBus]                 mem_wdata,
 
+    // to wb
     output reg[`RegAddrBus]             wb_wd,
     output reg                          wb_wreg,
     output reg[`RegBus]                 wb_wdata
@@ -15,13 +20,17 @@ module mem_wb(
 
 always @ (posedge clk) begin
     if (rst == `RstEnable)  begin
-        wb_wd <= `NOPRegAddr;
-        wb_wreg <= `WriteDisable;
-        wb_wdata <= `ZeroWord;
-    end else begin
-        wb_wd <= mem_wd;
-        wb_wreg <= mem_wreg;
-        wb_wdata <= mem_wdata;
+        wb_wd               <= `NOPRegAddr;
+        wb_wreg             <= `WriteDisable;
+        wb_wdata            <= `ZeroWord;
+    end else if (stall[5] == `Stop && stall[6] == `NoStop) begin
+        wb_wd               <= `NOPRegAddr;
+        wb_wreg             <= `WriteDisable;
+        wb_wdata            <= `ZeroWord;
+    end else if (stall[5] == `NoStop) begin
+        wb_wd               <= mem_wd;
+        wb_wreg             <= mem_wreg;
+        wb_wdata            <= mem_wdata;
     end
 end
 
